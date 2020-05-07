656af62517484e3e2a900560d74d794544c7d11b
snackersteph <swongwrites@gmail.com>
2020-05-06 22:53:37 -0700

    Complete containerWithMostWater

---
+++ Arrays/containerWithMostWater.js
@@ -0,0 +1,46 @@
+// Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). 
+// n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). 
+// Find two lines, which together with x-axis forms a container, such that the container contains 
+// the most water.
+
+// Note: You may not slant the container and n is at least 2.
+
+// The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area 
+// of water (blue section) the container can contain is 49.
+
+// Example:
+// Input: [1,8,6,2,5,4,8,3,7]
+// Output: 49
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+
+// Area is calulated by the length and the shortest side height
+
+// Start with the maximum width container and go to a shorter width container 
+// if there is a vertical line longer than the current containers shorter line. 
+// This way we are compromising on the width but we are looking forward to a 
+// longer length container.
+
+var maxArea = function(height) {
+    
+  let max = 0  
+  let left = 0
+  let right = height.length - 1
+  
+  while (left < right) {
+    let min = Math.min(height[left], height[right])
+    
+    max = Math.max(max, min * (right - left))
+    
+    if (height[left] < height[right]) {
+      left ++
+    } else {
+      right --
+    }
+  }
+  
+  return max
+};
