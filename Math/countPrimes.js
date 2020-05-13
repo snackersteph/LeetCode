7f658c555076717138ab40293d5833143657bb3b
snackersteph <swongwrites@gmail.com>
2020-05-13 11:28:28 -0700

    Complete countPrimes

---
+++ Math/countPrimes.js
@@ -0,0 +1,38 @@
+// Count the number of prime numbers less than a non-negative number, n.
+
+// Example:
+
+// Input: 10
+// Output: 4
+// Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+// Sieve of Eratosthenes
+var countPrimes = function(n) {
+  let primes = []
+
+  for (let i=0; i < n; i ++) {
+    primes[i] = true
+  }
+
+  // there are no primes greater than the current number and its square
+  // setting all the multiples of 2 to false
+  for (let i=2; i * i < primes.length; i++) {
+    if (primes[i]) {
+      for (let j=i; j * i < primes.length; j++) {
+        primes[i * j] = false
+      }
+    }
+  }
+
+  let primeCount = 0
+  for (let i=2; i < primes.length; i++) {
+    if (primes[i]) {
+      primeCount++
+    }
+  }
+  return primeCount
+};
